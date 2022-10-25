8659a20d540a5c250a0acb0a9b896ef2877af384
Alice Zoë Bevan–McGregor <amcgregor@cegid.com>
2022-10-25 10:48:49 -0400

    Indentation.

--- examples/widgets.py
+++ examples/widgets.py
@@ -8,103 +8,98 @@ from marrow.widgets import *
 
 
 if __name__ == '__main__':
-    search = Form('site-search', action='/search', method='get', children=[
-            SearchField('q', autofocus=True, autocomplete="on", placeholder="Site-wide search.")
-        ])
+	search = Form('site-search', action='/search', method='get', children=[
+			SearchField('q', autofocus=True, autocomplete="on", placeholder="Site-wide search.")
+		])
 
-    login = Form('sign-in', class_="tabbed", action='/users/action:authenticate', children=[
-            HiddenField('referrer'),
-            FieldSet('local', "Local Users", TableLayout, [
-                    TextField('identity', "User Name"),
-                    PasswordField('password', "Password")
-                ]),
-            FieldSet('openid', "OpenID Users", TableLayout, [
-                    URLField('url', "OpenID URL")
-                ])
-        ], footer=SubmitFooter('form', "Sign In"))
+	login = Form('sign-in', class_="tabbed", action='/users/action:authenticate', children=[
+			HiddenField('referrer'),
+			FieldSet('local', "Local Users", TableLayout, [
+					TextField('identity', "User Name"),
+					PasswordField('password', "Password")
+				]),
+			FieldSet('openid', "OpenID Users", TableLayout, [
+					URLField('url', "OpenID URL")
+				])
+		], footer=SubmitFooter('form', "Sign In"))
 
 
-    contact = Form('contact', action="/contact", children=[
-            FieldSet('contact', "Send us a Message", DefinitionListLayout, [
-                    TextField('name', "Your Name", required=True, autofocus=True),
-                    EmailField('email', "Your E-Mail Address", required=True),
-                    PhoneField('phone', "Your Phone Number"),
-                    TextArea('details', "Details")
-                ])
-        ], footer=SubmitFooter('form', "Send Message"))
+	contact = Form('contact', action="/contact", children=[
+			FieldSet('contact', "Send us a Message", DefinitionListLayout, [
+					TextField('name', "Your Name", required=True, autofocus=True),
+					EmailField('email', "Your E-Mail Address", required=True),
+					PhoneField('phone', "Your Phone Number"),
+					TextArea('details', "Details")
+				])
+		], footer=SubmitFooter('form', "Send Message"))
 
-    select = SelectField('myselect', values=[
-            (None, 'Default Template'),
-            ("Custom Templates", [
-                    ('home', "Homepage"),
-                    ('contact', "Contact Form")
-                ])
-        ])
+	select = SelectField('myselect', values=[
+			(None, 'Default Template'),
+			("Custom Templates", [
+					('home', "Homepage"),
+					('contact', "Contact Form")
+				])
+		])
 
-    select2 = SelectField('select2', size=10, values=[
-            (None, 'Default Template'),
-            ("Custom Templates", [
-                    ('home', "Homepage"),
-                    ('contact', "Contact Form")
-                ])
-        ])
+	select2 = SelectField('select2', size=10, values=[
+			(None, 'Default Template'),
+			("Custom Templates", [
+					('home', "Homepage"),
+					('contact', "Contact Form")
+				])
+		])
 
-    page = tag.html [
-            tag.head [
-                    tag.meta ( charset = "utf-8" ),
-                    tag.title [ "Example Widgets" ]
-                ],
-            
-            "\n",
-            
-            tag.body [
-                    tag.h1 [ "Example Widgets" ],
-                    "\n\n",
-                
-                    tag.h2 [ "Search" ],
-                    "\n",
-                    search(),
-                    "\n\n",
-                
-                    tag.h2 [ "Login" ],
-                    "\n",
-                    login(),
-                    "\n\n",
-                
-                    tag.h2 [ "Contact" ],
-                    "\n",
-                    contact(),
-                    "\n\n",
-                
-                    tag.h2 [ "Select Fields" ],
-                    "\n\n",
-                
-                    tag.h3 [ "Raw Select" ],
-                    "\n",
-                    select(),
-                    "\n\n",
-                
-                    tag.h3 [ "Raw Select w/ Data" ],
-                    "\n",
-                    select({'myselect': 'home'}),
-                    "\n\n",
-                
-                    tag.h3 [ "Raw Select, Large" ],
-                    "\n",
-                    select2(),
-                    "\n",
-                ]
-        ]
-    
-    print unicode(page)
-    
-    n = 5000
-    duration = Timer("list(page.render())", "from __main__ import page").timeit(n)
-    timeper = duration / float(n) * 1000
-    genper = float(n) / duration
-    print "Timeit (Widget Stream): %0.2fs for %d gens: %0.2f usec/gen (%d gen/sec)." % (duration, n, timeper, genper)
-    
-    duration = Timer("unicode(page)", "from __main__ import page").timeit(n)
-    timeper = duration / float(n) * 1000
-    genper = float(n) / duration
-    print "Timeit (Widget Monolithic): %0.2fs for %d gens: %0.2f usec/gen (%d gen/sec)." % (duration, n, timeper, genper)
+	page = tag.html [
+			tag.head [
+					tag.meta ( charset = "utf-8" ),
+					tag.title [ "Example Widgets" ]
+				],
+			
+			"\n",
+			
+			tag.body [
+					tag.h1 [ "Example Widgets" ],
+					"\n\n",
+				
+					tag.h2 [ "Search" ],
+					"\n",
+					search(),
+					"\n\n",
+				
+					tag.h2 [ "Login" ],
+					"\n",
+					login(),
+					"\n\n",
+				
+					tag.h2 [ "Contact" ],
+					"\n",
+					contact(),
+					"\n\n",
+				
+					tag.h2 [ "Select Fields" ],
+					"\n\n",
+				
+					tag.h3 [ "Raw Select" ],
+					"\n",
+					select(),
+					"\n\n",
+				
+					tag.h3 [ "Raw Select w/ Data" ],
+					"\n",
+					select({'myselect': 'home'}),
+					"\n\n",
+				
+					tag.h3 [ "Raw Select, Large" ],
+					"\n",
+					select2(),
+					"\n",
+				]
+		]
+	
+	print(str(page))
+	
+	n = 5000
+	duration = Timer("str(page)", "from __main__ import page").timeit(n)
+	timeper = duration / float(n) * 1000
+	genper = float(n) / duration
+	print("Timeit (Widget Monolithic): %0.2fs for %d gens: %0.2f usec/gen (%d gen/sec)." % (duration, n, timeper, genper))
--- marrow/widgets/layout.py
+++ marrow/widgets/layout.py
@@ -8,31 +8,31 @@ __all__ = ['DefinitionListLayout', 'TableLayout', 'SubmitFooter']
 
 
 class DefinitionListLayout(Layout):
-    @property
-    def template(self):
-        parts = []
-        
-        for child in self.children:
-            if not isinstance(child, Label):
-                parts.append(tag.dt [ self.label(unicode(child.name), for_=child)() ])
-            
-            parts.append(tag.dd [ child(self.data) ])
-        
-        return tag.dl ( class_ = "layout" ) [ parts ]
+	@property
+	def template(self):
+		parts = []
+		
+		for child in self.children:
+			if not isinstance(child, Label):
+				parts.append(tag.dt [ self.label(str(child.name), for_=child)() ])
+			
+			parts.append(tag.dd [ child(self.data) ])
+		
+		return tag.dl ( class_ = "layout" ) [ parts ]
 
 
 class TableLayout(Layout):
-    @property
-    def template(self):
-        return tag.table ( **self.args ) [ [(
-                tag.tr [ tag.th [ self.label(child.name, for_=child)() ],
-                tag.td [ child(self.data) ] ]
-            ) for child in self.children] ]
+	@property
+	def template(self):
+		return tag.table ( **self.args ) [ [(
+				tag.tr [ tag.th [ self.label(child.name, for_=child)() ],
+				tag.td [ child(self.data) ] ]
+			) for child in self.children] ]
 
 
 class SubmitFooter(Widget):
-    @property
-    def template(self):
-        return tag.div ( **self.args ) [
-                tag.input ( type = 'submit', name = self.name, value = self.title )
-            ]
+	@property
+	def template(self):
+		return tag.div ( **self.args ) [
+				tag.input ( type = 'submit', name = self.name, value = self.title )
+			]
