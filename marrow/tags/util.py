0838926a08c8c6a54a64a7d7e7d4e0283e3b6c08
Alice Zoë Bevan–McGregor <amcgregor@cegid.com>
2022-12-20 09:39:33 -0500

    Tab-based indentation.

--- marrow/tags/util.py
+++ marrow/tags/util.py
@@ -8,43 +8,43 @@ __all__ = ['quoteattrs', 'escape']
 
 
 def quoteattrs(context, attrs):
-    """Escape and quote a dict of attribute/value pairs.
-    
-    Escape &, <, and > in a string of data, then quote it for use as
-    an attribute value.  The " character will be escaped as well.
-    Also filter out None values.
-    """
-    for a, v in attrs.items():
-        if v is None or v is NoDefault:
-            continue
-        
-        if callable(v):
-            v = v(context)
-        
-        if isinstance(v, bool):
-            if v:
-                yield ' ' + a.strip(u'_')
-            
-            continue
-        
-        if not isinstance(v, unicode):
-            v = unicode(v)
-        
-        yield ' '  + a.strip(u'_').replace('_', '-') + '="'
-        
-        for s, r in [(u'&', u"&amp;"), (u">", u"&gt;"), (u"<", u"&lt;"), (u'"', u"&quot;")]:
-            try:
-                v = v.replace(s, r)
-            
-            except AttributeError:
-                raise AttributeError('Argument %s of %r can not be quoted.' % (a, context))
-        
-        yield v + '"'
+	"""Escape and quote a dict of attribute/value pairs.
+	
+	Escape &, <, and > in a string of data, then quote it for use as
+	an attribute value.  The " character will be escaped as well.
+	Also filter out None values.
+	"""
+	for a, v in attrs.items():
+		if v is None or v is NoDefault:
+			continue
+		
+		if callable(v):
+			v = v(context)
+		
+		if isinstance(v, bool):
+			if v:
+				yield ' ' + a.strip(u'_')
+			
+			continue
+		
+		if not isinstance(v, unicode):
+			v = unicode(v)
+		
+		yield ' '  + a.strip(u'_').replace('_', '-') + '="'
+		
+		for s, r in [(u'&', u"&amp;"), (u">", u"&gt;"), (u"<", u"&lt;"), (u'"', u"&quot;")]:
+			try:
+				v = v.replace(s, r)
+			
+			except AttributeError:
+				raise AttributeError('Argument %s of %r can not be quoted.' % (a, context))
+		
+		yield v + '"'
 
 
 def escape(v):
-    """Escape &, <, and > in a string of data."""
-    for s, r in [(u'&', u"&amp;"), (u">", u"&gt;"), (u"<", u"&lt;")]:
-        v = v.replace(s, r)
-    
-    return v
+	"""Escape &, <, and > in a string of data."""
+	for s, r in [(u'&', u"&amp;"), (u">", u"&gt;"), (u"<", u"&lt;")]:
+		v = v.replace(s, r)
+	
+	return v
